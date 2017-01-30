9c12978ee157e39b750215a9d2c24d18f2b12575
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-30 10:49:58 -0700

    :construction: create heroku app

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -14,25 +14,37 @@ const knex = require('../knex');
 router.get('/:id', (req, res, next) => {
   // GET /classifieds/:id should return the id,title, description, price and item_image of a single ad.
   const adId = parseInt(req.params.id);
-  knex('classifieds')
-    .where('classifieds.id', adId)
-    .first()
-    .then((ad) => {
-      delete ad.created_at;
-      delete ad.updated_at;
-      res.json(ad);
-    });
+  if (adId) {
+    knex('classifieds')
+      .where('classifieds.id', adId)
+      .first()
+      .then((ad) => {
+        if (ad) {
+          delete ad.created_at;
+          delete ad.updated_at;
+          res.json(ad);
+        } else {
+          next();
+        }
+      });
+  } else {
+    next();
+  }
 });
 
 router.get('/', (req, res, next) => {
   // GET /classifieds should return the id,title, description, price and item_image of all classifieds.
   knex('classifieds')
     .then((ads) => {
-      ads.forEach((ad) => {
-        delete ad.created_at;
-        delete ad.updated_at;
-      });
-      res.json(ads);
+      if (ads.length !== 0) {
+        ads.forEach((ad) => {
+          delete ad.created_at;
+          delete ad.updated_at;
+        });
+        res.json(ads);
+      } else {
+        next();
+      }
     });
 
 });
@@ -40,14 +52,18 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   // POST /classifieds should create a new ad and return the id, title, description, price and item_image that were created.
   const newAd = req.body;
-  knex('classifieds')
-    .insert(newAd, '*')
-    .then((ads) => {
-      const ad = ads[0];
-      delete ad.created_at;
-      delete ad.updated_at;
-      res.json(ad);
-    });
+  if (newAd) {
+    knex('classifieds')
+      .insert(newAd, '*')
+      .then((ads) => {
+        const ad = ads[0];
+        delete ad.created_at;
+        delete ad.updated_at;
+        res.json(ad);
+      });
+  } else {
+    next();
+  }
 
 });
 
@@ -83,10 +99,14 @@ router.delete('/:id', (req, res, next) => {
     .del()
     .returning('*')
     .then((ads) => {
-      const ad = ads[0];
-      delete ad.created_at;
-      delete ad.updated_at;
-      res.json(ad);
+      if (ads[0]) {
+        const ad = ads[0];
+        delete ad.created_at;
+        delete ad.updated_at;
+        res.json(ad);
+      } else {
+        next();
+      }
     });
 });
 
