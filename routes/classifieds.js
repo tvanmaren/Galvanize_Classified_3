b4d258d12a0a1b78f9ae66b0360e7f0cf68d3be7
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-30 10:23:22 -0700

    :sparkles: get:id route running

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -4,32 +4,42 @@ const express = require('express');
 
 const router = express.Router();
 
+const knex = require('../knex');
 
-router.get('/classifieds/:id', (req, res, next) => {
+// const {
+//   camelizeKeys,
+//   decamelizeKeys
+// } = require('humps');
+
+router.get('/:id', (req, res, next) => {
   // GET /classifieds/:id should return the id,title, description, price and item_image of a single ad.
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
+  knex('classifieds')
+    .where('classifieds.id', id)
+    .first()
+    .then((entry) => {
+      delete entry.created_at;
+      delete entry.updated_at;
+      res.json(entry);
+    });
 });
 
-
-router.get('/classifieds', (req, res, next) => {
+router.get('/', (req, res, next) => {
   // GET /classifieds should return the id,title, description, price and item_image of all classifieds.
 
 });
 
-
-router.post('/classifieds', (req, res, next) => {
+router.post('/', (req, res, next) => {
   // POST /classifieds should create a new ad and return the id, title, description, price and item_image that were created.
 
 });
 
-
-router.patch('/classifieds', (req, res, next) => {
+router.patch('/', (req, res, next) => {
   // PATCH /classifieds/:id should update an ad and return the id, title, description, price and item_image that were updated.
 
 });
 
-
-router.delete('/classifieds/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   // DELETE /classifieds/:id should delete an ad and return the id,title, description, price, and item_image that were deleted.
   const id = req.params.id;
 
