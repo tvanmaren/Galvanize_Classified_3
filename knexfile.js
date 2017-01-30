ce68a9921962208283058431e5e2551b265e4697
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-30 09:32:56 -0700

    :tada: DB connections setup

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,16 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
-
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/classifieds_dev'
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/classifieds_test'
+  },
+  production: {
+    client: 'pg',
+    database: process.env.DATABASE_URL
+  },
 };
