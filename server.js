1ca9308e38823e93ccf4d1a27e91ed2e16cf5479
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-30 11:30:18 -0700

    :construction: add angular & gulp setup to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,7 +8,15 @@ const bodyParser = require('body-parser');
 const messages = require('./routes/classifieds');
 
 app.use(bodyParser.json());
-app.use('/classifieds',messages);
+app.use(express.static('node_modules/angular'));
+app.use(express.static('public'));
+app.use('/classifieds', messages);
+
+app.use('*', (req, res, next) => {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, 'public')
+  });
+});
 
 const port = process.env.PORT || 3000;
 
