2a807866b6c0828a341f4bfe260497553b837cb8
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-30 09:45:38 -0700

    :construction: migration successful

--- migrations/20170130092155_classifieds.js
+++ migrations/20170130092155_classifieds.js
@@ -1,8 +1,19 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+/* eslint-disable no-unused-vars*/
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('classifieds', (table) => {
+    table.increments();
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('price').notNullable();
+    table.string('item_image').notNullable();
+    table.timestamps(true, true);
+  });
+
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('classifieds');
 };
