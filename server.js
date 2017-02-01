be83ddb22929f0ce8f8c07af64e5b50a4d62af0c
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-31 23:07:35 -0700

    :bug: remove roboto from server & add font-awesome to accessible directory

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.use('/classifieds', messages);
 
 app.use('/angular', express.static(path.join(__dirname, '/node_modules/angular/angular.min.js')));
 app.use('/angular-ui-router', express.static(path.join(__dirname, '/node_modules/angular-ui-router/release/angular-ui-router.min.js')));
-app.use('/font-awesome', express.static(path.join(__dirname, '/node_modules/font-awesome/css/font-awesome.min.css')));
-app.use('/roboto-fontface', express.static(path.join(__dirname, '/node_modules/roboto-fontface/css/roboto/roboto-fontface.css')));
+app.use('/font-awesome', express.static(path.join(__dirname, '/fonts/font-awesome-4.7.0/css/font-awesome.min.css')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('*', (req, res) => {
